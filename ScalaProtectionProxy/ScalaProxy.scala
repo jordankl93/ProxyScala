6746690ee461c7d52e89d6f3382699e04264c405
jordankl93 <jordankl93@gmail.com>
2015-02-24 22:06:26 -0300

    Update ScalaProxy.scala

--- ScalaProtectionProxy/ScalaProxy.scala
+++ ScalaProtectionProxy/ScalaProxy.scala
@@ -27,18 +27,18 @@ class BancoUsuarios{
 class BancoProxyScala(usuario: String, senha: String) extends BancoUsuarios {
 
 	override def getNumeroUsuarios(): String = {
-        if (temPermissaoDeAcesso()) {
-            return super.getNumeroUsuarios()
-        }
-        return "Login incorreto"
-    }
+	        if (temPermissaoDeAcesso()) {
+	            return super.getNumeroUsuarios()
+	        }
+	        return "Login incorreto"
+    	}
 	
 	override def getUsuariosConectados(): String = {
-        if (temPermissaoDeAcesso()) {
-            return super.getUsuariosConectados()
-        }
-        return "Login incorreto"
-    }
+	        if (temPermissaoDeAcesso()) {
+	            return super.getUsuariosConectados()
+	        }
+	        return "Login incorreto"
+    	}
 	
 	def temPermissaoDeAcesso(): Boolean = (usuario == "admin") && (senha == "admin")
 	
@@ -48,13 +48,13 @@ class BancoProxyScala(usuario: String, senha: String) extends BancoUsuarios {
 object ScalaProxy {
 	def main(args: Array[String]) {
 		Console.println("Hacker acessando")
-        var banco = new BancoProxyScala("admin", "1234")
-        Console.println(banco.getNumeroUsuarios())
-        Console.println(banco.getUsuariosConectados())
-
-        Console.println("\nAdministrador acessando")
-        banco = new BancoProxyScala("admin", "admin")
-        Console.println(banco.getNumeroUsuarios())
-        Console.println(banco.getUsuariosConectados())
+	        var banco = new BancoProxyScala("admin", "1234")
+	        Console.println(banco.getNumeroUsuarios())
+	        Console.println(banco.getUsuariosConectados())
+	
+	        Console.println("\nAdministrador acessando")
+	        banco = new BancoProxyScala("admin", "admin")
+	        Console.println(banco.getNumeroUsuarios())
+	        Console.println(banco.getUsuariosConectados())
 	}
 }
